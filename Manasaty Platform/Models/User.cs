ca597f82f81f5cd13ccadcc2a6a0d73b483c864c
AmrHalima <amr158halima@icloud.com>
2024-11-30 21:54:18 +0200

    Add files via upload
    Defining Project internal Structure

---
+++ Manasaty Platform/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manasaty_Platform.Models
+{
+    internal abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public enum Gender
+        {
+            Male,Female
+        }
+        public Gender gender { get; set; }
+        protected User(int id,string name,string email,string password,Gender gender,string phone)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            this.gender = gender;
+            Phone = phone;
+        }
+
+    }
+}
