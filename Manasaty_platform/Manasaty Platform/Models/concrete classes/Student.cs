f6197a9b1b6e7454b2fa580256eff97f25e00edc
Ahmed Khalifa <154266453+Ahmed3khalifa@users.noreply.github.com>
2024-12-02 00:36:04 +0200

    Add files via upload
    Create Login design

---
+++ Manasaty_platform/Manasaty Platform/Models/concrete classes/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manasaty_Platform.Models
+{
+    internal class Student:User
+    {
+       
+        private string ParentPhone {  get; set; }
+        
+        public Student(int id, string name, string email, string password, Gender gender,string phone,string parentPhone) :base(id,name,email,password,gender, phone)
+        {
+            
+            ParentPhone = parentPhone;
+        }
+
+    }
+}
---
+++ Manasaty_platform/Manasaty Platform/Models/concrete classes/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Manasaty_Platform.Models.User;
+
+namespace Manasaty_Platform.Models
+{
+    internal class Teacher:User
+    {
+        public Teacher(int id, string name, string email, string password, Gender gender, string phone) : base(id, name, email, password, gender,phone)
+        { }
+    }
+}
